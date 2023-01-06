0d05bbee79b718d940b4574f682055071705d349
HangenYuu <97870091+HangenYuu@users.noreply.github.com>
2023-01-06 20:39:03 +0800

    added helper_functions

---
+++ ARCHITECTURE/CNN/helper/tiny_vgg.py
@@ -0,0 +1,29 @@
+from torch import nn
+
+class TinyConvNet(nn.Module):
+    def __init__(self, input_shape, hidden_units, output_shape):
+        super().__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units*7*7, output_shape)
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.classifier(x)
+        return x
